ed5fc3110f80640fbb94f489d1602391d50b5ba6
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-03-09 02:02:31 +0530

    Add files via upload

---
+++ 01 Int Variable.java
@@ -0,0 +1,18 @@
+package Tim;
+
+public class Hello {
+    public static void main(String[]args){
+
+        System.out.println("Hello, Tim");
+        int myFirstNumber = (10 + 5) + (2 * 10);//variable
+        int mySecondNumber = 12; //47
+        int myThirdNumber = myFirstNumber * 2; //70
+        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+        int myLastOne = 1000;
+        System.out.println(myLastOne-myTotal);
+
+
+
+
+    }
+}
