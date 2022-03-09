246e039ee3cb1ea18b7b9e918b8b628de8ae9ca5
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-03-10 00:23:01 +0530

    Add files via upload

---
+++ 05 CharAndBoolean.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char myChar = 'D';
+        char myUnicodeValue = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeValue);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        //Boolean
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+
+    }
+}
