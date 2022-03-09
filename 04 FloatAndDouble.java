1af1d9ecd50aa6274308acab653ebc6851b58222
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-03-10 00:22:09 +0530

    Add files via upload

---
+++ 04 FloatAndDouble.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myIntValue = 5/2;
+        float myFloatValue =  5.25f/2f;
+        double myDoubleValue = 5.25d/2f;
+        System.out.println(myIntValue);
+        System.out.println(myFloatValue);
+        System.out.println(myDoubleValue);
+
+
+
+        double myPounds = 200d;
+        double myKilogram = myPounds * 0.45359237d;
+        System.out.println("Kilogram is = "+myKilogram);
+
+        double miles = 300d;
+        double kilometer = miles * 1.609d;
+        System.out.println("Kilometer is = "+kilometer);
+
+
+    }
+}
