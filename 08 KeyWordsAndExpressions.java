32e661e1d27b8c6423b2366b5af562c98bd75144
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-03-11 02:10:24 +0530

    Add files via upload

---
+++ 08 KeyWordsAndExpressions.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int score = 100; // here score 100 is an expression
+        if (score>99) // here score >99 is an expression
+        {
+            System.out.println("You got the high score!"); //here inside double quotes symbol " " is an expression
+        }
+        score=0; //here score =0 is an expression
+
+    }
+}
