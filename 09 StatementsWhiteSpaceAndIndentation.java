8e1702be2aabedba23be13f860f1f42403cfc098
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-03-12 02:31:00 +0530

    Add files via upload

---
+++ 09 StatementsWhiteSpaceAndIndentation.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50; // this is a statement
+        if (myVariable == 50) {
+            System.out.println("This is also a statement"); //This too is a statement
+        }
+
+
+    }
+
+}
