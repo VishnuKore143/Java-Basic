c260d94497c2dbe0698c48efb4fd11769c00d694
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-03-15 23:25:51 +0530

    Add files via upload

---
+++ 17 Coding Exercise 4 Leap Year Calculator.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static boolean isLeapYear(int year){
+        if(year < 1 && year > 9999){
+            return false;
+        } else if((year % 4 == 0 && year % 100 != 0) && (year >= 1 && year <= 9999)|| year % 400 == 0 && (year >= 1 && year <= 9999)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    }
+
