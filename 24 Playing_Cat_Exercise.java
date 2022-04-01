7b0c3f9b9c360271b1cfbfdb545bf5faea52ab02
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-04-02 00:19:00 +0530

    Add files via upload

---
+++ 24 Playing_Cat_Exercise.java
@@ -0,0 +1,23 @@
+public class Playing_Cat_Exercise {
+    public static void main(String[] args) {
+       isCatPlaying(true,30);
+    }
+        public static boolean isCatPlaying(boolean summer, int temperature){
+            if (summer == true) {
+                if (temperature >= 25 && temperature <= 45) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                if (temperature >= 25 && temperature <= 35) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    }
