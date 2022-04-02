6a03b88b343ff6fb71c293df4c59fd7ecfa9fec6
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-04-03 01:33:57 +0530

    Add files via upload

---
+++ 29 ForLoop.java
@@ -0,0 +1,43 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i=1; i<=8; i++){
+            //System.out.println("10,000 at "+i+"% interest "+calculateInterest(10000.0,i));
+        }
+        System.out.println();
+        for (int i=8; i>=2; i--) {
+            //System.out.println("10,000 at "+i+"% interest "+calculateInterest(10000.0,i));
+        }
+
+        //isPrime Method
+        int count = 0;
+        for (int i=10; i<50; i++){
+            if (isPrime(i)){
+                count++;
+                System.out.println("Number "+i+" is a prime number");
+                if (count==10){
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+
+        }
+    }
+    public static double calculateInterest(double amount, double interestRate){
+        return (amount*(interestRate/100));
+    }
+
+    public static boolean isPrime(int n){
+        if (n==1){
+            return false;
+        }
+        for (int i=2; i<= (long) Math.sqrt(n); i++){
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+}
