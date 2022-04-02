5d6353a8620799a6395cb75d4fb0eb15fb84a15a
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-04-02 13:36:43 +0530

    Update 25 Switch_Statement.java

--- 25 Switch_Statement.java
+++ 25 Switch_Statement.java
@@ -1,5 +1,6 @@
 public class Switch_Statement {
     public static void main(String[] args) {
+        //char example
         char charValue = 'F';
         switch(charValue){
             case 'A':
@@ -14,5 +15,22 @@ public class Switch_Statement {
             default:
                 System.out.println("Could not found the Value A,B,C,D or E");
         }
+        
+        //int example
+        int numValue = 1;
+        switch(numValue){
+            case 1:
+                System.out.println("1 was found");
+                break;
+            case 2:
+                System.out.println("2 was found");
+                break:
+            case 3: case 4: case 5:
+                System.out.println("3, 4, or 5 was found");
+                break;
+            default:
+                System.out.println("Value not found");
+                break;
+        } 
     }
 }
