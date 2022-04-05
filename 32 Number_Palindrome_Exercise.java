10e349f3090b447299f3c74242bb2e81580c9cb5
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2022-04-05 13:27:16 +0530

    Add files via upload

---
+++ 32 Number_Palindrome_Exercise.java
@@ -0,0 +1,28 @@
+package Udemy;
+
+public class Number_Palindrome_Exercise {
+    public static void main(String[] args) {
+
+ boolean ans = isPalindrome(2052);
+        System.out.println(ans);
+    }
+    public static boolean isPalindrome(int number){
+
+        if(number < 0) {
+            number = -number;
+        }
+        int temp = number;
+        int rev = 0;
+        while(number>0){
+            int lastDigit = number%10;
+            rev = rev*10+lastDigit;
+            number /= 10; // number = number / 10;
+        }
+        if(rev == temp){
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+}
